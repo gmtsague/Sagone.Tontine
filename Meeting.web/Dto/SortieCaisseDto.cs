ce01102af221ab062fde7ac05855a53544c0d29b
Geovani TSAGUE <gmtsague@gmail.com>
2023-06-07 18:04:49 +0100

    1- Mise à jour des libelles définis via DataAnnotation 2- Remplacement de la référence à la classe <EngagementDto> par celle de la classe <RubriqueDto>

--- Meeting.web/Dto/SortieCaisseDto.cs
+++ Meeting.web/Dto/SortieCaisseDto.cs
@@ -15,71 +15,73 @@ namespace Meeting.Web.Dto
         /// <summary>
         /// Identifiant de l&apos;inscription
         /// </summary>
-        [Display( Name="idinscrit")]
+        [Display( Name="Bénéficiare")]
         public int? Idinscrit { get; set; }
 
         /// <summary>
         /// Seance_id
         /// </summary>
-        [Display( Name="seance_id")]
+        [Display( Name="Seance")]
         public int SeanceId { get; set; }
 
         /// <summary>
         /// Engagement_id
         /// </summary>
-        [Display( Name="engagement_id")]
-        public int EngagementId { get; set; }
+        [Display( Name="Rubrique")]
+        public int RubriqueId { get; set; }
 
         /// <summary>
         /// No de Reference permettant d''identifier le document
         /// </summary>
-        [Display(Name = "ref_no")]
+        [Display(Name = "NoRef.Doc")]
         [StringLength(128)]
         public string? RefNo { get; set; }
 
         /// <summary>
         /// Benef_principal
         /// </summary>
-        [Display(Name = "benef_principal")]
+        [Display(Name = "Benef. Principal")]
         public bool BenefPrincipal { get; set; }
 
         /// <summary>
         /// Date effective a laquelle a lieu l&apos;evenement
         /// </summary>
-        [Display( Name="dateevts")]
+        [Display( Name="Date Evts")]
         public DateOnly Dateevts { get; set; }
 
         /// <summary>
         /// Montant percu par le beneficiaire de l&apos;evenement
         /// </summary>
-        [Display( Name="montantpercu")]
+        [Display( Name="Montant percu")]
         public decimal Montantpercu { get; set; }
 
         /// <summary>
         /// Observation generale concernant le deroulement de l&apos;evenement
         /// </summary>
-        [Display( Name="note")]
+        [Display( Name="Observations")]
         [StringLength(1024)]
         public string? Note { get; set; }
 
         /// <summary>
         /// Indique que l&apos;evenement a ete cloture (pret solde, ou toutes les participations atteintes pour un evenement)
         /// </summary>
-        [Display( Name="is_closed")]
+        [Display( Name="Is Closed")]
         public bool IsClosed { get; set; }
 
         /// <summary>
         /// Nombre de signatures restantes avant cloture du document ou de la seance de cotisation.
         /// </summary>
-        [Display( Name="visarestants")]
+        [Display( Name="Visas restants")]
         public int Visarestants { get; set; }
 
-        public virtual EngagementDto Engagement { get; set; } = null!;
+        public virtual RubriqueDto? Rubrique { get; set; }
 
+        [Display(Name = "Bénéficiaire")]
         public virtual InscriptionDto? IdinscritNavigation { get; set; }
 
-       // public virtual ICollection<MeetVisa> MeetVisas { get; set; } = new List<MeetVisa>();
-
-        public virtual SeanceDto Seance { get; set; } = null!;
+        // public virtual ICollection<MeetVisa> MeetVisas { get; set; }
+        
+        [Display(Name = "Séance")]
+        public virtual SeanceDto? Seance { get; set; }
     }
 }
