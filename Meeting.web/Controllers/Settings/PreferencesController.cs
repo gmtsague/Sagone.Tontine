b2a57d39b7c1cc5964f6a913bb250acc4f999dd4
Geovani TSAGUE <gmtsague@gmail.com>
2023-06-15 10:02:29 +0100

    Mise à jour du code en utilisant l'implémentation du repository PreferenceRepository.

--- Meeting.web/Controllers/Settings/PreferencesController.cs
+++ Meeting.web/Controllers/Settings/PreferencesController.cs
@@ -7,51 +7,52 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using MeetingEntities.Models;
 using Meeting.Web.Dto;
+using Meeting.Web.Repository;
+using Mapster;
+using FormHelper;
 
 namespace Meeting.web.Controllers.Settings
 {
     public class PreferencesController : Controller
     {
-        private readonly LabosContext _context;
+       // private readonly LabosContext _context;
+
+        private readonly IPreferencesRepository _repository;
 
         private readonly ILogger<PreferencesController> _logger;
 
-        public PreferencesController(ILogger<PreferencesController> logger, LabosContext context)
+        public PreferencesController(ILogger<PreferencesController> logger, IPreferencesRepository repository)
         {
             _logger = logger;
-            _context = context;
+            //_context = context;
+            _repository = repository;
         }
 
         // GET: Preferences
         public async Task<IActionResult> Index()
         {
-            var labosContext = _context.MeetPreferences.Include(m => m.Setting);
-            return View(await labosContext.ToListAsync());
+            //var labosContext = _context.MeetPreferences.Include(m => m.Setting);
+            //return View(await labosContext.ToListAsync());
+            var resultItems = await _repository.GetAll();
+            return View(resultItems.Items.AsQueryable().ProjectToType<PreferenceDto>().ToList());
         }
 
         // GET: Preferences/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || _context.MeetPreferences == null)
+            var findObj = await _repository.GetDetails(id);
+            if (findObj == null)
             {
-                return NotFound();
+                return FormResult.CreateErrorResult(UtilityController.RequestedEntityNotFound);
             }
 
-            var meetPreference = await _context.MeetPreferences
-                .Include(m => m.Setting)
-                .FirstOrDefaultAsync(m => m.SettingId == id);
-            if (meetPreference == null)
-            {
-                return NotFound();
-            }
-
-            return View(meetPreference);
+            return View(findObj.Adapt<PreferenceDto>());
         }
 
         // GET: Preferences/Create
         public IActionResult Create()
         {
-            ViewData["SettingId"] = new SelectList(_context.CoreAnnualSettings, "SettingId", "SettingId");
+            //ViewData["SettingId"] = new SelectList(_context.CoreAnnualSettings, "SettingId", "SettingId");
             return View();
         }
 
@@ -60,33 +61,34 @@ namespace Meeting.web.Controllers.Settings
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("SettingId,TauxInteretMensuel,TauxInteretPenalite,TauxPenaliteCotisation,EnableAutoGenPresence,EnableSigningOutcome,EnableMaxDelayPenalites,EnableAutoDispatchIncome,EnableFixedAmountFees,EnableSecoursInsurance,EnableFixedFeesByAnten")] PreferenceDto meetPreference)
+        public async Task<IActionResult> Create(/*[Bind("SettingId,TauxInteretMensuel,TauxInteretPenalite,TauxPenaliteCotisation,EnableAutoGenPresence,EnableSigningOutcome,EnableMaxDelayPenalites,EnableAutoDispatchIncome,EnableFixedAmountFees,EnableSecoursInsurance,EnableFixedFeesByAnten")]*/ PreferenceDto valueDto)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(meetPreference);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                var Preference = valueDto.ToEntity();
+
+                int SavedElts = await _repository.Add(Preference);
+
+                if (SavedElts > 0)
+                    // return RedirectToAction(nameof(Index));
+                    return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
+                else
+                    return FormResult.CreateErrorResult("Echec on saved entity.");
             }
-            ViewData["SettingId"] = new SelectList(_context.CoreAnnualSettings, "SettingId", "SettingId", meetPreference.SettingId);
-            return View(meetPreference);
+            //ViewData["SettingId"] = new SelectList(_context.CoreAnnualSettings, "SettingId", "SettingId", meetPreference.SettingId);
+            return View(valueDto);
         }
 
         // GET: Preferences/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.MeetPreferences == null)
+            var findObj = await _repository.GetDetails(id);
+            if (findObj == null)
             {
-                return NotFound();
+                return FormResult.CreateErrorResult(UtilityController.RequestedEntityNotFound);
             }
-
-            var meetPreference = await _context.MeetPreferences.FindAsync(id);
-            if (meetPreference == null)
-            {
-                return NotFound();
-            }
-            ViewData["SettingId"] = new SelectList(_context.CoreAnnualSettings, "SettingId", "SettingId", meetPreference.SettingId);
-            return View(meetPreference);
+            //ViewData["SettingId"] = new SelectList(_context.CoreAnnualSettings, "SettingId", "SettingId", findObj.SettingId);
+            return View(findObj.Adapt<PreferenceDto>());
         }
 
         // POST: Preferences/Edit/5
@@ -94,54 +96,37 @@ namespace Meeting.web.Controllers.Settings
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("SettingId,TauxInteretMensuel,TauxInteretPenalite,TauxPenaliteCotisation,EnableAutoGenPresence,EnableSigningOutcome,EnableMaxDelayPenalites,EnableAutoDispatchIncome,EnableFixedAmountFees,EnableSecoursInsurance,EnableFixedFeesByAnten")] PreferenceDto meetPreference)
+        public async Task<IActionResult> Edit(int id, [Bind("SettingId,TauxInteretMensuel,TauxInteretPenalite,TauxPenaliteCotisation,EnableAutoGenPresence,EnableSigningOutcome,EnableMaxDelayPenalites,EnableAutoDispatchIncome,EnableFixedAmountFees,EnableSecoursInsurance,EnableFixedFeesByAnten")] PreferenceDto valueDto)
         {
-            if (id != meetPreference.SettingId)
+            if (id != valueDto.SettingId)
             {
                 return NotFound();
             }
 
             if (ModelState.IsValid)
             {
-                try
-                {
-                    _context.Update(meetPreference);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MeetPreferenceExists(meetPreference.SettingId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
+                var preference = valueDto.ToEntity();
+                int SavedElts = await _repository.Update(id, preference);
+                if (SavedElts > 0)
+                    //return RedirectToAction(nameof(Index));
+                    return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
+                else
+                    return FormResult.CreateErrorResult("Echec on saved entity.");
             }
-            ViewData["SettingId"] = new SelectList(_context.CoreAnnualSettings, "SettingId", "SettingId", meetPreference.SettingId);
-            return View(meetPreference);
+            //ViewData["SettingId"] = new SelectList(_context.CoreAnnualSettings, "SettingId", "SettingId", meetPreference.SettingId);
+            return View(valueDto);
         }
 
         // GET: Preferences/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.MeetPreferences == null)
+            var findObj = await _repository.GetDetails(id);
+            if (findObj == null)
             {
-                return NotFound();
+                return FormResult.CreateErrorResult(UtilityController.RequestedEntityNotFound);
             }
 
-            var meetPreference = await _context.MeetPreferences
-                .Include(m => m.Setting)
-                .FirstOrDefaultAsync(m => m.SettingId == id);
-            if (meetPreference == null)
-            {
-                return NotFound();
-            }
-
-            return View(meetPreference);
+            return View(findObj.Adapt<PreferenceDto>());
         }
 
         // POST: Preferences/Delete/5
@@ -149,23 +134,23 @@ namespace Meeting.web.Controllers.Settings
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.MeetPreferences == null)
+            var findObj = await _repository.GetDetails(id);
+            if (findObj == null)
             {
-                return Problem("Entity set 'LabosContext.MeetPreferences'  is null.");
+                return FormResult.CreateErrorResult(UtilityController.RequestedEntityNotFound);
             }
-            var meetPreference = await _context.MeetPreferences.FindAsync(id);
-            if (meetPreference != null)
-            {
-                _context.MeetPreferences.Remove(meetPreference);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+            int SavedElts = await _repository.Delete(id);
+            if (SavedElts > 0)
+                //return RedirectToAction(nameof(Index));
+                return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
+            else
+                return FormResult.CreateErrorResult(UtilityController.DeleteOperationFailed);
         }
 
-        private bool MeetPreferenceExists(int id)
-        {
-          return (_context.MeetPreferences?.Any(e => e.SettingId == id)).GetValueOrDefault();
-        }
+        //private bool MeetPreferenceExists(int id)
+        //{
+        //  return (_context.MeetPreferences?.Any(e => e.SettingId == id)).GetValueOrDefault();
+        //}
     }
 }
