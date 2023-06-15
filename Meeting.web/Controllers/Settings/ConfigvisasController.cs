39b4fd5e5af027e99dbfc5c5f901255e9e149d11
Geovani TSAGUE <gmtsague@gmail.com>
2023-06-15 10:01:21 +0100

    Mise à jour du code en utilisant l'implémentation du repository ConfigvisasRepository.

--- Meeting.web/Controllers/Settings/ConfigvisasController.cs
+++ Meeting.web/Controllers/Settings/ConfigvisasController.cs
@@ -9,19 +9,23 @@ using MeetingEntities.Models;
 using Mapster;
 using Meeting.Web.Dto;
 using FormHelper;
+using Meeting.Web.Repository;
 
 namespace Meeting.web.Controllers.Settings
 {
     public class ConfigvisasController : Controller
     {
-        private readonly LabosContext _context;
+       // private readonly LabosContext _context;
+
+        private readonly IConfigvisasRepository _repository;
 
         private readonly ILogger<ConfigvisasController> _logger;
 
-        public ConfigvisasController(ILogger<ConfigvisasController> logger, LabosContext context)
+        public ConfigvisasController(ILogger<ConfigvisasController> logger, IConfigvisasRepository repository)
         {
             _logger = logger;
-            _context = context;
+            // _context = context;
+            _repository = repository;
         }
 
         // GET: Configvisas
@@ -31,37 +35,54 @@ namespace Meeting.web.Controllers.Settings
 
             TypeAdapterConfig<MeetConfigVisa, ConfigvisaDto>.NewConfig().MaxDepth(3);
 
-            var labosContext = _context.MeetConfigVisas.Include(m => m.Annee).Include(m => m.Poste).Include(m => m.Typerub);
-            return View(await labosContext.AsQueryable().ProjectToType<ConfigvisaDto>().ToListAsync());
+            var resultItems = await _repository.GetAll();
+            return View(resultItems.Items.AsQueryable().ProjectToType<ConfigvisaDto>().ToList());
+
+            // var labosContext = _context.MeetConfigVisas.Include(m => m.Annee).Include(m => m.Poste).Include(m => m.Typerub);
+            // return View(await labosContext.AsQueryable().ProjectToType<ConfigvisaDto>().ToListAsync());
+        }
+
+        private void SetViewDataElements(ConfigvisaDto? valueDto)
+        {
+            ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_repository.GetUnitOfWork(), valueDto?.AnneeId ?? 0);
+            ViewData["PosteId"] = UtilityController.GetSelectListOfPostes(_repository.GetUnitOfWork(), valueDto?.PosteId ?? 0);
+            ViewData["TyperubId"] = UtilityController.GetSelectListOfTypeRubriques(_repository.GetUnitOfWork(), valueDto?.TyperubId ?? 0);
         }
 
         // GET: Configvisas/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || _context.MeetConfigVisas == null)
-            {
-                return NotFound();
-            }
+            //if (id == null || _context.MeetConfigVisas == null)
+            //{
+            //    return NotFound();
+            //}
 
-            var meetConfigVisa = await _context.MeetConfigVisas
-                .Include(m => m.Annee)
-                .Include(m => m.Poste)
-                .Include(m => m.Typerub)
-                .FirstOrDefaultAsync(m => m.ConfigvisaId == id);
-            if (meetConfigVisa == null)
-            {
-                return NotFound();
-            }
+            //var meetConfigVisa = await _context.MeetConfigVisas
+            //    .Include(m => m.Annee)
+            //    .Include(m => m.Poste)
+            //    .Include(m => m.Typerub)
+            //    .FirstOrDefaultAsync(m => m.ConfigvisaId == id);
+            //if (meetConfigVisa == null)
+            //{
+            //    return NotFound();
+            //}
 
-            return PartialView("Details", meetConfigVisa.Adapt<ConfigvisaDto>());
+            var findObj = await _repository.GetDetails(id);
+            if (findObj == null)
+            {
+                // return NotFound();
+                return FormResult.CreateErrorResult(UtilityController.RequestedEntityNotFound);
+            }
+            return PartialView("Details", findObj.Adapt<ConfigvisaDto>());
         }
 
         // GET: Configvisas/Create
         public IActionResult Create()
         {
-            ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context);
-            ViewData["PosteId"] = UtilityController.GetSelectListOfPostes(_context);
-            ViewData["TyperubId"] = UtilityController.GetSelectListOfTypeRubriques(_context);
+            //ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context);
+            //ViewData["PosteId"] = UtilityController.GetSelectListOfPostes(_context);
+            //ViewData["TyperubId"] = UtilityController.GetSelectListOfTypeRubriques(_context);
+            SetViewDataElements(null);
             return PartialView("Create");
         }
 
@@ -71,39 +92,57 @@ namespace Meeting.web.Controllers.Settings
         [HttpPost]
         [ValidateAntiForgeryToken]
         [FormValidator]
-        public async Task<IActionResult> Create([Bind("ConfigvisaId,PosteId,AnneeId,TyperubId,Numordre,SignByOrdre")] ConfigvisaDto valueDto)
+        public async Task<IActionResult> Create(/*[Bind("ConfigvisaId,PosteId,AnneeId,TyperubId,Numordre,SignByOrdre")]*/ ConfigvisaDto valueDto)
         {
             if (ModelState.IsValid)
             {
                 var meetConfigVisa = valueDto.ToEntity();
-                _context.Add(meetConfigVisa);
-                await _context.SaveChangesAsync();
-                // return RedirectToAction(nameof(Index));
-                return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
+
+                int SavedElts = await _repository.Add(meetConfigVisa);
+                if (SavedElts > 0)
+                    //return RedirectToAction(nameof(Index));
+                    return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
+                else
+                    return FormResult.CreateErrorResult("Echec on saved entity.");
+                //_context.Add(meetConfigVisa);
+                //await _context.SaveChangesAsync();
+                //// return RedirectToAction(nameof(Index));
+                //return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
             }
-            ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context, valueDto.AnneeId);
-            ViewData["PosteId"] = UtilityController.GetSelectListOfPostes(_context, valueDto.PosteId);
-            ViewData["TyperubId"] = UtilityController.GetSelectListOfTypeRubriques(_context, valueDto.TyperubId);
+            //ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context, valueDto.AnneeId);
+            //ViewData["PosteId"] = UtilityController.GetSelectListOfPostes(_context, valueDto.PosteId);
+            //ViewData["TyperubId"] = UtilityController.GetSelectListOfTypeRubriques(_context, valueDto.TyperubId);
+            SetViewDataElements(valueDto);
             return PartialView("Create",valueDto);
         }
 
         // GET: Configvisas/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.MeetConfigVisas == null)
+            //if (id == null || _context.MeetConfigVisas == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //var meetConfigVisa = await _context.MeetConfigVisas.FindAsync(id);
+            //if (meetConfigVisa == null)
+            //{
+            //    return NotFound();
+            //}
+            //ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context, meetConfigVisa.AnneeId);
+            //ViewData["PosteId"] = UtilityController.GetSelectListOfPostes(_context, meetConfigVisa.PosteId);
+            //ViewData["TyperubId"] = UtilityController.GetSelectListOfTypeRubriques(_context, meetConfigVisa.TyperubId);
+
+            var findObj = await _repository.GetDetails(id);
+            if (findObj == null)
             {
-                return NotFound();
+                // return NotFound();
+                return FormResult.CreateErrorResult(UtilityController.RequestedEntityNotFound);
             }
 
-            var meetConfigVisa = await _context.MeetConfigVisas.FindAsync(id);
-            if (meetConfigVisa == null)
-            {
-                return NotFound();
-            }
-            ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context, meetConfigVisa.AnneeId);
-            ViewData["PosteId"] = UtilityController.GetSelectListOfPostes(_context, meetConfigVisa.PosteId);
-            ViewData["TyperubId"] = UtilityController.GetSelectListOfTypeRubriques(_context, meetConfigVisa.TyperubId);
-            return PartialView("Edit", meetConfigVisa.Adapt<ConfigvisaDto>());
+            var valueDto = findObj.Adapt<ConfigvisaDto>();
+            SetViewDataElements(valueDto);
+            return PartialView("Edit", valueDto);
         }
 
         // POST: Configvisas/Edit/5
@@ -112,7 +151,7 @@ namespace Meeting.web.Controllers.Settings
         [HttpPost]
         [ValidateAntiForgeryToken]
         [FormValidator]
-        public async Task<IActionResult> Edit(int id, [Bind("ConfigvisaId,PosteId,AnneeId,TyperubId,Numordre,SignByOrdre")] ConfigvisaDto valueDto)
+        public async Task<IActionResult> Edit(int id, /*[Bind("ConfigvisaId,PosteId,AnneeId,TyperubId,Numordre,SignByOrdre")]*/ ConfigvisaDto valueDto)
         {
             if (id != valueDto.ConfigvisaId)
             {
@@ -121,51 +160,64 @@ namespace Meeting.web.Controllers.Settings
 
             if (ModelState.IsValid)
             {
-                 var meetConfigVisa = valueDto.ToEntity();                
-                try
-                {
-                    _context.Update(meetConfigVisa);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MeetConfigVisaExists(meetConfigVisa.ConfigvisaId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                // return RedirectToAction(nameof(Index));
-                return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
+                var meetConfigVisa = valueDto.ToEntity();
+                int SavedElts = await _repository.Update(id, meetConfigVisa);
+                if (SavedElts > 0)
+                    //return RedirectToAction(nameof(Index));
+                    return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
+                else
+                    return FormResult.CreateErrorResult("Echec on saved entity.");
+                //try
+                //{
+                //    _context.Update(meetConfigVisa);
+                //    await _context.SaveChangesAsync();
+                //}
+                //catch (DbUpdateConcurrencyException)
+                //{
+                //    if (!MeetConfigVisaExists(meetConfigVisa.ConfigvisaId))
+                //    {
+                //        return NotFound();
+                //    }
+                //    else
+                //    {
+                //        throw;
+                //    }
+                //}
+                //// return RedirectToAction(nameof(Index));
+                //return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
             }
-            ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context, valueDto.AnneeId);
-            ViewData["PosteId"] = UtilityController.GetSelectListOfPostes(_context, valueDto.PosteId);
-            ViewData["TyperubId"] = UtilityController.GetSelectListOfTypeRubriques(_context, valueDto.TyperubId);
+            //ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context, valueDto.AnneeId);
+            //ViewData["PosteId"] = UtilityController.GetSelectListOfPostes(_context, valueDto.PosteId);
+            //ViewData["TyperubId"] = UtilityController.GetSelectListOfTypeRubriques(_context, valueDto.TyperubId);
+            SetViewDataElements(valueDto);
             return PartialView("Edit", valueDto);
         }
 
         // GET: Configvisas/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.MeetConfigVisas == null)
-            {
-                return NotFound();
-            }
+            //if (id == null || _context.MeetConfigVisas == null)
+            //{
+            //    return NotFound();
+            //}
 
-            var meetConfigVisa = await _context.MeetConfigVisas
-                .Include(m => m.Annee)
-                .Include(m => m.Poste)
-                .Include(m => m.Typerub)
-                .FirstOrDefaultAsync(m => m.ConfigvisaId == id);
-            if (meetConfigVisa == null)
-            {
-                return NotFound();
-            }
+            //var meetConfigVisa = await _context.MeetConfigVisas
+            //    .Include(m => m.Annee)
+            //    .Include(m => m.Poste)
+            //    .Include(m => m.Typerub)
+            //    .FirstOrDefaultAsync(m => m.ConfigvisaId == id);
+            //if (meetConfigVisa == null)
+            //{
+            //    return NotFound();
+            //}
 
-            return PartialView("Delete", meetConfigVisa.Adapt<ConfigvisaDto>());
+            var findObj = await _repository.GetDetails(id);
+            if (findObj == null)
+            {
+                // return NotFound();
+                return FormResult.CreateErrorResult(UtilityController.RequestedEntityNotFound);
+            }
+            return PartialView("Delete", findObj.Adapt<ConfigvisaDto>());
         }
 
         // POST: Configvisas/Delete/5
@@ -174,25 +226,39 @@ namespace Meeting.web.Controllers.Settings
         [FormValidator]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.MeetConfigVisas == null)
+            //if (_context.MeetConfigVisas == null)
+            //{
+            //    // return Problem("Entity set 'LabosContext.MeetConfigVisas'  is null.");
+            //    return FormResult.CreateErrorResult(UtilityController.DeleteOperationFailed);
+            //}
+            //var meetConfigVisa = await _context.MeetConfigVisas.FindAsync(id);
+            //if (meetConfigVisa != null)
+            //{
+            //    _context.MeetConfigVisas.Remove(meetConfigVisa);
+            //}
+
+            //await _context.SaveChangesAsync();
+            //// return RedirectToAction(nameof(Index));
+            //return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
+
+            var findObj = await _repository.GetDetails(id);
+            if (findObj == null)
             {
-                // return Problem("Entity set 'LabosContext.MeetConfigVisas'  is null.");
+                // return NotFound();
+                return FormResult.CreateErrorResult(UtilityController.RequestedEntityNotFound);
+            }
+
+            int SavedElts = await _repository.Delete(id);
+            if (SavedElts > 0)
+                //return RedirectToAction(nameof(Index));
+                return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
+            else
                 return FormResult.CreateErrorResult(UtilityController.DeleteOperationFailed);
-            }
-            var meetConfigVisa = await _context.MeetConfigVisas.FindAsync(id);
-            if (meetConfigVisa != null)
-            {
-                _context.MeetConfigVisas.Remove(meetConfigVisa);
-            }
-            
-            await _context.SaveChangesAsync();
-            // return RedirectToAction(nameof(Index));
-            return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
         }
 
-        private bool MeetConfigVisaExists(int id)
-        {
-          return (_context.MeetConfigVisas?.Any(e => e.ConfigvisaId == id)).GetValueOrDefault();
-        }
+        //private bool MeetConfigVisaExists(int id)
+        //{
+        //  return (_context.MeetConfigVisas?.Any(e => e.ConfigvisaId == id)).GetValueOrDefault();          
+        //}
     }
 }
