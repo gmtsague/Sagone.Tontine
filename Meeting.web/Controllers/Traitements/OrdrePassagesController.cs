49fbf4da76f22ad75b330518e6323c0e766e0fb8
Geovani TSAGUE <gmtsague@gmail.com>
2023-06-15 10:04:53 +0100

    Mise à jour du code en utilisant l'implémentation du repository OrdrepassageRepository.

--- Meeting.web/Controllers/Traitements/OrdrePassagesController.cs
+++ Meeting.web/Controllers/Traitements/OrdrePassagesController.cs
@@ -9,19 +9,23 @@ using MeetingEntities.Models;
 using Meeting.Web.Dto;
 using Mapster;
 using FormHelper;
+using Meeting.Web.Repository;
 
 namespace Meeting.web.Controllers.Traitements
 {
     public class OrdrePassagesController : Controller
     {
-        private readonly LabosContext _context;
+        //private readonly LabosContext _context;
+
+        private readonly IOrdrePassageRepository _repository;
 
         private readonly ILogger<OrdrePassagesController> _logger;
 
-        public OrdrePassagesController(ILogger<OrdrePassagesController> logger, LabosContext context)
+        public OrdrePassagesController(ILogger<OrdrePassagesController> logger, IOrdrePassageRepository repository)
         {
             _logger = logger;
-            _context = context;
+            //_context = context;
+            _repository = repository;
         }
 
         // GET: OrdrePassages
@@ -29,16 +33,20 @@ namespace Meeting.web.Controllers.Traitements
         {
             ViewData["TitleObj"] = new FormTitle("Ordre de passages");
 
-            ViewData["AntennesData"] = UtilityController.GetSelectListOfAntennes(_context, Convert.ToInt64(TempData.Peek("SelectedEtab") ?? 0) /*UtilityController.GetGlobalSelectedAssociation()*/);
+            ViewData["AntennesData"] = UtilityController.GetSelectListOfAntennes(_repository.GetUnitOfWork(), Convert.ToInt64(TempData.Peek("SelectedEtab") ?? 0) /*UtilityController.GetGlobalSelectedAssociation()*/);
 
-            var labosContext = _context.MeetOrdrePassages
-                                        .Include(m => m.CoreSubdivision)
-                                        .Include(m => m.IdinscritNavigation.MeetAntenne)
-                                        //.ThenInclude(m=>m.Person)
-                                        .Include(p => p.IdinscritNavigation.Person)
-                                        .Where(m => m.IdinscritNavigation.EtabId == Convert.ToInt64(TempData.Peek("SelectedEtab") ?? 0))
-                                        .Where(m => m.AnneeId == Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0));
-            return View(await labosContext.OrderBy(m=>m.CoreSubdivision.MonthDate).ProjectToType<OrdrePassageDto>().ToListAsync());
+            //var labosContext = _context.MeetOrdrePassages
+            //                            .Include(m => m.CoreSubdivision)
+            //                            .Include(m => m.IdinscritNavigation.MeetAntenne)
+            //                            //.ThenInclude(m=>m.Person)
+            //                            .Include(p => p.IdinscritNavigation.Person)
+            //                            .Where(m => m.IdinscritNavigation.EtabId == Convert.ToInt64(TempData.Peek("SelectedEtab") ?? 0))
+            //                            .Where(m => m.AnneeId == Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0));
+            //return View(await labosContext.OrderBy(m=>m.CoreSubdivision.MonthDate).ProjectToType<OrdrePassageDto>().ToListAsync());
+
+            var resultItems = await _repository.GetAll( (int)Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0),
+                                                     (int)Convert.ToInt64(TempData.Peek("SelectedEtab") ?? 0) );
+            return View( resultItems.Items.AsQueryable().ProjectToType<OrdrePassageDto>().ToList());
         }
 
         // GET: OrdrePassages
@@ -48,41 +56,45 @@ namespace Meeting.web.Controllers.Traitements
 
             //iewData["AntennesData"] = UtilityController.GetSelectListOfAntennes(_context, Convert.ToInt64(TempData.Peek("SelectedEtab") ?? 0) /*UtilityController.GetGlobalSelectedAssociation()*/);
 
-            var labosContext = _context.MeetOrdrePassages.Include(m => m.CoreSubdivision)
-                                                         .Include(m => m.IdinscritNavigation.MeetAntenne)
-                                                         //.ThenInclude(m=>m.MeetAntenne)
-                                                         .Include(p => p.IdinscritNavigation.Person)
-                                                         .Where(m => m.IdinscritNavigation.EtabId == Convert.ToInt64(TempData.Peek("SelectedEtab") ?? 0))
-                                                         .Where(m => (Id <= 0 || m.IdinscritNavigation.AntenneId == Id) && m.AnneeId == Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0));
-            return PartialView("_PartialGridViewPassages", await labosContext.OrderBy(m => m.CoreSubdivision.MonthDate).AsNoTracking().ProjectToType<OrdrePassageDto>().ToListAsync());
+            //var labosContext = _context.MeetOrdrePassages.Include(m => m.CoreSubdivision)
+            //                                             .Include(m => m.IdinscritNavigation.MeetAntenne)
+            //                                             //.ThenInclude(m=>m.MeetAntenne)
+            //                                             .Include(p => p.IdinscritNavigation.Person)
+            //                                             .Where(m => m.IdinscritNavigation.EtabId == Convert.ToInt64(TempData.Peek("SelectedEtab") ?? 0))
+            //                                             .Where(m => (Id <= 0 || m.IdinscritNavigation.AntenneId == Id) && m.AnneeId == Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0));
+            //return PartialView("_PartialGridViewPassages", await labosContext.OrderBy(m => m.CoreSubdivision.MonthDate).AsNoTracking().ProjectToType<OrdrePassageDto>().ToListAsync());
+
+            var resultItems = await _repository.GetByAntenne(Id, (int)Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0),
+                                                     (int)Convert.ToInt64(TempData.Peek("SelectedEtab") ?? 0));
+            return PartialView("_PartialGridViewPassages", resultItems.Items.AsQueryable().ProjectToType<OrdrePassageDto>().ToList());
+        }
+
+        private void SetViewDataElements(OrdrePassageDto? valueDto)
+        {
+            ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_repository.GetUnitOfWork(), valueDto.AnneeId);
+            ViewData["DivisionId"] = UtilityController.GetSelectListOfYearSubdivisions(_repository.GetUnitOfWork(), UtilityController.GetGlobalSelectedYear(), valueDto?.DivisionId ?? 0);
+            ViewData["Idinscrit"] = UtilityController.GetSelectListOfInscriptions(_repository.GetUnitOfWork(), Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0), valueDto?.Idinscrit ?? 0);
         }
 
         // GET: OrdrePassages/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null || _context.MeetOrdrePassages == null)
+            var findObj = await _repository.GetDetails(id);
+            if (findObj == null)
             {
-                return NotFound();
+                //return NotFound();
+                return FormResult.CreateErrorResult(UtilityController.RequestedEntityNotFound);
             }
-
-            var meetOrdrePassage = await _context.MeetOrdrePassages
-                .Include(m => m.CoreSubdivision)
-                .Include(m => m.IdinscritNavigation).ThenInclude(m=>m.Person)
-                .FirstOrDefaultAsync(m => m.PassageId == id);
-            if (meetOrdrePassage == null)
-            {
-                return NotFound();
-            }
-
-            return PartialView("Details", meetOrdrePassage.Adapt<OrdrePassageDto>());
+            return PartialView("Details", findObj.Adapt<OrdrePassageDto>());
         }
 
         // GET: OrdrePassages/Create
         public IActionResult Create()
         {
-            ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context);
-            ViewData["DivisionId"] = UtilityController.GetSelectListOfYearSubdivisions(_context, UtilityController.GetGlobalSelectedYear());
-            ViewData["Idinscrit"] = UtilityController.GetSelectListOfInscriptions(_context, Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0));
+            //ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context);
+            //ViewData["DivisionId"] = UtilityController.GetSelectListOfYearSubdivisions(_context, UtilityController.GetGlobalSelectedYear());
+            //ViewData["Idinscrit"] = UtilityController.GetSelectListOfInscriptions(_context, Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0));
+            SetViewDataElements(null);
             var valueDto = new OrdrePassageDto() 
             { 
                 AnneeId = (int)Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0) 
@@ -96,39 +108,41 @@ namespace Meeting.web.Controllers.Traitements
         [HttpPost]
         [ValidateAntiForgeryToken]
         [FormValidator]
-        public async Task<IActionResult> Create([Bind("PassageId,AnneeId,DivisionId,Idinscrit,Montantpercu,Heuredebut,Commentaire")] OrdrePassageDto valueDto)
+        public async Task<IActionResult> Create(/*[Bind("PassageId,AnneeId,DivisionId,Idinscrit,Montantpercu,Heuredebut,Commentaire")]*/ OrdrePassageDto valueDto)
         {
             if (ModelState.IsValid)
             {
-                MeetOrdrePassage meetOrdrePassage = valueDto.ToEntity();
-                _context.Add(meetOrdrePassage);
-                await _context.SaveChangesAsync();
-                //return RedirectToAction(nameof(Index));
-                return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
+                MeetOrdrePassage OrdrePassage = valueDto.ToEntity();
+
+                int SavedElts = await _repository.Add( OrdrePassage);
+                if (SavedElts > 0)
+                    //return RedirectToAction(nameof(Index));
+                    return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
+                else
+                    return FormResult.CreateErrorResult("Echec on saved entity.");
             }
-            ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context, valueDto.AnneeId);
-            ViewData["DivisionId"] = UtilityController.GetSelectListOfYearSubdivisions(_context, UtilityController.GetGlobalSelectedYear());
-            ViewData["Idinscrit"] = UtilityController.GetSelectListOfInscriptions(_context, Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0), (long)valueDto.Idinscrit);
+            //ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context, valueDto.AnneeId);
+            //ViewData["DivisionId"] = UtilityController.GetSelectListOfYearSubdivisions(_context, UtilityController.GetGlobalSelectedYear());
+            //ViewData["Idinscrit"] = UtilityController.GetSelectListOfInscriptions(_context, Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0), (long)valueDto.Idinscrit);
+            SetViewDataElements(valueDto);
             return PartialView("Create", valueDto);
         }
 
         // GET: OrdrePassages/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null || _context.MeetOrdrePassages == null)
+            var findObj = await _repository.GetDetails(id);
+            if (findObj == null)
             {
-                return NotFound();
+                //return NotFound();
+                return FormResult.CreateErrorResult(UtilityController.RequestedEntityNotFound);
             }
-
-            var meetOrdrePassage = await _context.MeetOrdrePassages.FindAsync(id);
-            if (meetOrdrePassage == null)
-            {
-                return NotFound();
-            }
-            ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context, meetOrdrePassage.AnneeId);
-            ViewData["DivisionId"] = UtilityController.GetSelectListOfYearSubdivisions(_context, UtilityController.GetGlobalSelectedYear(), meetOrdrePassage.DivisionId);
-            ViewData["Idinscrit"] = UtilityController.GetSelectListOfInscriptions(_context, Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0), (long)meetOrdrePassage.Idinscrit);
-            return PartialView("Edit", meetOrdrePassage.Adapt<OrdrePassageDto>());
+            //ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context, meetOrdrePassage.AnneeId);
+            //ViewData["DivisionId"] = UtilityController.GetSelectListOfYearSubdivisions(_context, UtilityController.GetGlobalSelectedYear(), meetOrdrePassage.DivisionId);
+            //ViewData["Idinscrit"] = UtilityController.GetSelectListOfInscriptions(_context, Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0), (long)meetOrdrePassage.Idinscrit);
+            var valueDto = findObj.Adapt<OrdrePassageDto>();
+            SetViewDataElements(valueDto);
+            return PartialView("Edit", valueDto);
         }
 
         // POST: OrdrePassages/Edit/5
@@ -137,7 +151,7 @@ namespace Meeting.web.Controllers.Traitements
         [HttpPost]
         [ValidateAntiForgeryToken]
         [FormValidator]
-        public async Task<IActionResult> Edit(int id, [Bind("PassageId,AnneeId,DivisionId,Idinscrit,Montantpercu,Heuredebut,Commentaire")] OrdrePassageDto valueDto)
+        public async Task<IActionResult> Edit(int id, /*[Bind("PassageId,AnneeId,DivisionId,Idinscrit,Montantpercu,Heuredebut,Commentaire")]*/ OrdrePassageDto valueDto)
         {
             if (id != valueDto.PassageId)
             {
@@ -146,50 +160,35 @@ namespace Meeting.web.Controllers.Traitements
 
             if (ModelState.IsValid)
             {
-                MeetOrdrePassage meetOrdrePassage = valueDto.ToEntity();               
-                try
-                {
-                    _context.Update(meetOrdrePassage);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MeetOrdrePassageExists(meetOrdrePassage.PassageId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
+                MeetOrdrePassage OrdrePassage = valueDto.ToEntity();
+
+                int SavedElts = await _repository.Update(id, OrdrePassage);
+                if (SavedElts > 0)
+                    //return RedirectToAction(nameof(Index));
+                    return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
+                else
+                    return FormResult.CreateErrorResult("Echec on saved entity.");
                 //return RedirectToAction(nameof(Index));
                 return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
             }
-            ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context, valueDto.AnneeId);
-            ViewData["DivisionId"] = UtilityController.GetSelectListOfYearSubdivisions(_context, UtilityController.GetGlobalSelectedYear(), valueDto.DivisionId);
-            ViewData["Idinscrit"] = UtilityController.GetSelectListOfInscriptions(_context, Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0), (long)valueDto.Idinscrit);
+            //ViewData["AnneeId"] = UtilityController.GetSelectListOfAnnees(_context, valueDto.AnneeId);
+            //ViewData["DivisionId"] = UtilityController.GetSelectListOfYearSubdivisions(_context, UtilityController.GetGlobalSelectedYear(), valueDto.DivisionId);
+            //ViewData["Idinscrit"] = UtilityController.GetSelectListOfInscriptions(_context, Convert.ToInt64(TempData.Peek("SelectedYear") ?? 0), (long)valueDto.Idinscrit);
+            SetViewDataElements(valueDto);
             return PartialView("Edit", valueDto);
         }
 
         // GET: OrdrePassages/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null || _context.MeetOrdrePassages == null)
+            var findObj = await _repository.GetDetails(id);
+            if (findObj == null)
             {
-                return NotFound();
+                //return NotFound();
+                return FormResult.CreateErrorResult(UtilityController.RequestedEntityNotFound);
             }
 
-            var meetOrdrePassage = await _context.MeetOrdrePassages
-                .Include(m => m.CoreSubdivision)
-                .Include(m => m.IdinscritNavigation).ThenInclude(m => m.Person)
-                .FirstOrDefaultAsync(m => m.PassageId == id);
-            if (meetOrdrePassage == null)
-            {
-                return NotFound();
-            }
-
-            return PartialView("Delete", meetOrdrePassage.Adapt<OrdrePassageDto>());
+            return PartialView("Delete", findObj.Adapt<OrdrePassageDto>());
         }
 
         // POST: OrdrePassages/Delete/5
@@ -198,25 +197,24 @@ namespace Meeting.web.Controllers.Traitements
         [FormValidator]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            if (_context.MeetOrdrePassages == null)
+            var findObj = await _repository.GetDetails(id);
+            if (findObj == null)
             {
-                // return Problem("Entity set 'LabosContext.MeetOrdrePassages'  is null.");
+                //return NotFound();
+                return FormResult.CreateErrorResult(UtilityController.RequestedEntityNotFound);
+            }
+
+            int SavedElts = await _repository.Delete(id);
+            if (SavedElts > 0)
+                //return RedirectToAction(nameof(Index));
+                return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
+            else
                 return FormResult.CreateErrorResult(UtilityController.DeleteOperationFailed);
-            }
-            var meetOrdrePassage = await _context.MeetOrdrePassages.FindAsync(id);
-            if (meetOrdrePassage != null)
-            {
-                _context.MeetOrdrePassages.Remove(meetOrdrePassage);
-            }
-            
-            await _context.SaveChangesAsync();
-            //return RedirectToAction(nameof(Index));
-            return FormResult.CreateSuccessResult(UtilityController.SuccessOperation, Url.Action(nameof(Index)));
         }
 
-        private bool MeetOrdrePassageExists(int id)
-        {
-          return (_context.MeetOrdrePassages?.Any(e => e.PassageId == id)).GetValueOrDefault();
-        }
+        //private bool MeetOrdrePassageExists(int id)
+        //{
+        //  return (_context.MeetOrdrePassages?.Any(e => e.PassageId == id)).GetValueOrDefault();
+        //}
     }
 }
